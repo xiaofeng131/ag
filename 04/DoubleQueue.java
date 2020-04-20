c48745595f8a4711d919cee125a7194904dc0d6f
thebud15 <thebud15@sina.com>
2020-04-21 01:20:57 +0800

    use idea

---
+++ 04/DoubleQueue.java
@@ -0,0 +1,7 @@
+public class DoubleQueue {
+    public static void main(final String[] args) {
+        System.out.println("a2");
+
+    }
+
+}
---
+++ src/_1/Helloworld.java
@@ -0,0 +1,7 @@
+package _1;
+
+public class Helloworld {
+    public static void main(String[] args) {
+                System.out.println("hello world");
+    }
+}
