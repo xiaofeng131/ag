e4bef822b335409c7adb2402320f46713b07dbb8
thebud15 <thebud15@sina.com>
2020-04-21 20:57:13 +0800

    delete primary test

--- 03/DoubleQueue.java
+++
@@ -1,7 +0,0 @@
-public class DoubleQueue {
-    public static void main(final String[] args) {
-        System.out.println("a2");
-
-    }
-
-}
---
+++ src/_8/getNearLess.java
@@ -0,0 +1,5 @@
+package _8;
+
+public class getNearLess {
+
+}
